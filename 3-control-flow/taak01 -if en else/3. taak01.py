009385fac9764cc091adcf02e2545a12afee5c22
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-11-09 13:24:43 +0100

    3. taak01 af

---
+++ 3-control-flow/taak01 -if en else/3. taak01.py
@@ -0,0 +1,25 @@
+#opdracht1
+a = 6
+if (a == 5):
+  print("a is gelijk aan 5")
+else:
+  print("a is niet gelijk aan 5")
+
+
+  #opdracht2
+
+
+
+
+  #opdracht3
+a = 6
+if (a == 0):
+  print("a is gelijk aan 0")
+elif (a == 1):
+  print("a is gelijk aan 1")
+elif (a == 2):
+  print("a is gelijk aan 2") 
+elif (a == 3):
+  print("a is gelijk aan 3")  
+else:
+  print("a is niet gelijk aan 0, 1, 2 of 3")
