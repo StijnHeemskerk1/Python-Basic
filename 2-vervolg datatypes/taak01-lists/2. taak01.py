19fa0d91343c769fc1eb7c54dc2624fb57e36c06
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-11-09 12:13:10 +0100

    2. taak01 af

---
+++ 2-vervolg datatypes/taak01-lists/2. taak01.py
@@ -0,0 +1,13 @@
+#vraag 1. Strings
+#vraag 2. 7
+
+#opgave1.
+maanden = ['januari','februari','maart','april','mei','juni','july','augustus','september','oktober','november','december']
+maanden[6] = 'juli'
+#opgave2.
+#print(*maanden, sep = "\n")
+
+#opgave3.
+maanden = ['januari','februari','maart','april','mei','juni','july','augustus','september','oktober','november','december']
+for x in maanden:
+  print(x)
